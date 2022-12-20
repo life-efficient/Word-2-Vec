7b541f9db00f73a57eb00193aee4f8647cc633f9
Harry Berg <harryaberg@gmail.com>
2022-12-20 21:54:45 +0000

    Gets first model working end-to-end. No results shown yet

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 
 
 import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
 
 # TODO make dataset
 
 
 class TextDataset(torch.utils.data.Dataset):
     def __init__(self):
-
-        corpus = "The wide road shimmered in the hot sun".lower().split()
+        self.corpus = "The wide road shimmered in the hot sun".lower().split()
+        unique_words = set(self.corpus)
+        self.idx_to_token = {idx: token for idx,
+                             token in enumerate(unique_words)}
+        self.token_to_idx = {token: idx for idx,
+                             token in self.idx_to_token.items()}
         window_start_idx = 0
         window_size = 3
         assert window_size % 2 == 1
         self.skipgrams = []
-        while window_start_idx + window_size < len(corpus):
-            window = corpus[window_start_idx:window_start_idx+window_size]
+        while window_start_idx + window_size < len(self.corpus):
+            window = self.corpus[window_start_idx:window_start_idx+window_size]
             central_word = window.pop(int((window_size-1)/2))
             skip_grams_for_this_window = []
             for context_word in window:
@@ -28,41 +34,60 @@ class TextDataset(torch.utils.data.Dataset):
         return len(self.skipgrams)
 
     def __getitem__(self, idx):
-        return self.skipgrams[idx]
-# TODO make dataloader
+        central_word, context_word = self.skipgrams[idx]
+        central_word_idx = self.token_to_idx[central_word]
+        context_word_idx = self.token_to_idx[context_word]
+        return central_word_idx, context_word_idx
+
+# DEFINE WORD2VEC
 
 
-class SkipGram(torch.nn.Module):
+class Word2Vec(torch.nn.Module):
     def __init__(self, n_words, embedding_dim):
         super().__init__()
-        self.embeddings = torch.nn.Embedding(n_words, embedding_dim)
-        self.decoder = torch.nn.Linear(embedding_dim, n_words)
+        self.central_word_embeddings = torch.nn.Embedding(
+            n_words, embedding_dim)
+        self.context_word_embeddings = torch.nn.Linear(
+            embedding_dim, n_words, bias=False)
 
-    def forward(self, one_hot_central_word):
-        embedding = self.embeddings(one_hot_central_word)
-        logits_predicted_word = self.decoder(embedding)
-        return logits_predicted_word
+    def forward(self, X):
+        print(X)
+        central_word_embedding = self.central_word_embeddings(X)
+        similarities = self.context_word_embeddings(central_word_embedding)
+
+        # cwe = self.context_word_embeddings.parameters()
+        # similarities = torch.matmul(cwe.T, central_word_embedding)
+
+        return similarities
+        # probability_dist_over_context_words = F.softmax(similarities)
+        # return probability_dist_over_context_words
 
 
 def train(model, dataloader, epochs=10):
     optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
     for epoch in range(epochs):
         for batch in dataloader:
-            central_word, context_word = batch
-            print(central_word)
-            print(context_word)
-            predicted_context_word = skipgram(central_word)
-            loss = torch.nn.functional.cross_entropy(
-                predicted_context_word, context_word)
-            loss.backward()
-            optimiser.step()
-            optimiser.zero_grad()
-            print("Loss:", loss.item())
+            word_indices_of_central_word_for_each_example_in_batch, labels = batch
+            similarities = model(
+                word_indices_of_central_word_for_each_example_in_batch)  # predict
+            loss = F.cross_entropy(similarities, labels)  # calculat loss
+            loss.backward()  # calculates gradients by doing backprop
+            optimiser.step()  # take optimisation step
+            optimiser.zero_grad()  # zero grad
+            print(loss.item())
 
 
 dataset = TextDataset()
-dataloader = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size=4)
-skipgram = SkipGram(n_words=1000, embedding_dim=16)
-train(skipgram, dataloader)
+train_len = round(0.8*len(dataset))
+val_len = round(0.1*len(dataset))
+test_len = len(dataset) - val_len - train_len
+lengths = [train_len, val_len, test_len]
+train_set, val_set, test_set = random_split(dataset, lengths)
+# dataloader shuffles and batches the dataset
+train_loader = DataLoader(train_set, batch_size=4, shuffle=True)
+
+word2vec = Word2Vec(n_words=len(dataset.idx_to_token), embedding_dim=128)
+
+train(word2vec, train_loader)
 
 # %%
